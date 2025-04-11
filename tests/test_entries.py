a0e2dec1961201ee100f31f1f541538739f5bdce
Ekaterina Balabaeva <balabaeva.e.yu@yandex.ru>
2025-04-11 06:09:16 +0300

    add test with mark to skip

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -1,6 +1,13 @@
+import pytest
+
 from src.base_classes.response import Response
 from src.pydantic.item import Item
 
 
 def test_getting_items(get_items):
     Response(get_items).assert_status_code(200).validate(Item)
+
+
+@pytest.mark.skip('test skipping')
+def test_for_skip():
+    print('need to skip')
