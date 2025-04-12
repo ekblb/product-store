39b7103ee1e2fb64c634336265d75026fbf0a52c
Ekaterina Balabaeva <balabaeva.e.yu@yandex.ru>
2025-04-12 11:41:42 +0300

    add case with deleting keys in generator

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -20,8 +20,9 @@ def test_for_skip():
 
 
 @pytest.mark.development
-@pytest.mark.parametrize('first_value, second_value, result', [
-    (1, 2, 3), (-1, 3, 2), ('a', 3, None)
+@pytest.mark.parametrize(
+    'first_value, second_value, result', [
+        (1, 2, 3), (-1, 3, 2), ('a', 3, None)
         ])
 def test_function_in_fixture(first_value, second_value, result,
                              function_fixture):
@@ -31,9 +32,23 @@ def test_function_in_fixture(first_value, second_value, result,
     assert function_fixture(first_value, second_value) == result
 
 
-@pytest.mark.parametrize('text_in_message', ['Small message', '', None])
+@pytest.mark.parametrize(
+        'text_in_message', ['Small message', '', None]
+        )
 def test_generator_messages(text_in_message, get_message_generator):
     """
     Try to use generator in test
     """
     print(get_message_generator.set_text(text_in_message).build())
+
+
+@pytest.mark.parametrize(
+        'delete_key', ['email', 'name', 'text']
+        )
+def test_messages_without_key(delete_key, get_message_generator):
+    """
+    Delete keys in generator
+    """
+    message_without_key = get_message_generator.build()
+    del message_without_key[delete_key]
+    print(message_without_key)
